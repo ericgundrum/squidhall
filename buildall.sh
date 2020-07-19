e6b56571e883ef6662e2a27111ffbb4f8735f5ef
Jack William Bell <jack.bell@conzealand.nz>
2020-07-19 11:55:33 -0700

    Initial work for breakout rooms. Moved squidhall content from world module file to room-squidhall module file, added room module files for breakout rooms, reworked scripts and HTML files. Also changed gitignore to not include generated lib files.

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script generates code for all modules and merges the content files.
 python3 tools/sqs/sqs.py generate world.module.json
+python3 tools/sqs/sqs.py generate room-squidhall.module.json
+python3 tools/sqs/sqs.py generate room-wheketere.module.json
+python3 tools/sqs/sqs.py generate room-idiosepius.module.json
 python3 tools/sqs/sqs.py generate furniture.module.json
 python3 tools/sqs/sqs.py generate content/*.content.module.json
 python3 tools/sqs/sqs.py filter libs/modules/ MergeContentJS libs/modules/content/*.js
--- libs/squidmods/squidcommon.js
+++ libs/squidmods/squidcommon.js
@@ -11,11 +11,31 @@ var SQUIDCOMMON = function() {
 	};
 	
 	var attachLoaderHooks = function(squidSpace){
-		SQUIDSPACE.attachLoaderHook("hookname",
-			function(areaName, areaOrigin, config, placeName, data, objName, meshes, scene) {
+		SQUIDSPACE.attachLoaderHook("BasicRoom",
+			function(objName, options, data, scene) {
 				
-				squidSpace.logDebug(`hookname called! ${areaName}, ${areaOrigin}, ${config}, ${placeName}, ${data}`);
-			// function body.
+				squidSpace.logDebug(`BasicRoom called! ${areaName}, ${areaOrigin}, ${config}, ${placeName}, ${data}`);
+				
+				// Get options.
+				
+				// Get data.
+				let size = [4, 2.5, 6];
+				if (data["size"]) {
+					size = data["size"];
+				}
+				let floorMat = undefined;
+				if (data["floor-material"]) {
+					floorMat = data["floor-material"];
+				}
+				let wallMat = undefined;
+				if (data["wall-material"]) {
+					wallMat = data["wall-material"];
+				}
+				let ceilingMat = undefined;
+				if (data["ceiling-material"]) {
+					ceilingMat = data["ceiling-material"];
+				}
+				
 	    });
 	};
 	
